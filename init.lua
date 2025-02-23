a23ade9f1fce4eb4f89dca94f447c2df3e69dcaa
Rodrigo Pavano <rodrigopavano@gmail.com>
2025-02-23 12:03:58 -0300

    Autoimport plugins folder

--- init.lua
+++ init.lua
@@ -972,15 +972,7 @@ require("lazy").setup({
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
 
-	require("plugins.neo-tree"),
-	require("plugins.gitsigns"),
-	require("plugins.mini-comment"),
-
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	-- { import = 'custom.plugins' },
+	{ import = "plugins" },
 	--
 	-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
 	-- Or use telescope!
