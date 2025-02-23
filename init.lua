459d4078b150edb8629254478e8591f9ef1ad30c
Rodrigo Pavano <rodrigopavano@gmail.com>
2025-02-23 11:35:42 -0300

    Format buffer key

--- init.lua
+++ init.lua
@@ -734,7 +734,7 @@ require("lazy").setup({
 		cmd = { "ConformInfo" },
 		keys = {
 			{
-				"<leader>f",
+				"<leader>F",
 				function()
 					require("conform").format({ async = true, lsp_format = "fallback" })
 				end,
